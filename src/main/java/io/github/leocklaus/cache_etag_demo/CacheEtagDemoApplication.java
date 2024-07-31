0d7ad2cc7190b866cb95245291b5bec2f2ca3400
leocklaus <leocklaus@gmail.com>
2024-07-31 08:59:27 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/cache_etag_demo/CacheEtagDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.cache_etag_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacheEtagDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacheEtagDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/cache_etag_demo/CacheEtagDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.cache_etag_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacheEtagDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
