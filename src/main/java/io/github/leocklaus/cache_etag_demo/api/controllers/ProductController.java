a39a22b7a79852d82f93fa579c6d9ff3b643ed68
leocklaus <leocklaus@gmail.com>
2024-07-31 11:42:55 -0300

    feat: cache control and deep etag added

--- src/main/java/io/github/leocklaus/cache_etag_demo/api/controllers/ProductController.java
+++ src/main/java/io/github/leocklaus/cache_etag_demo/api/controllers/ProductController.java
@@ -9,8 +9,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.CacheControl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -21,15 +27,30 @@ public class ProductController {
     private final ProductService productService;
 
     @GetMapping
-    public ResponseEntity<Page<Product>> getAllProductsPaged(Pageable pageable){
+    public ResponseEntity<Page<Product>> getAllProductsPaged(
+            Pageable pageable,
+            ServletWebRequest request){
+
+        ShallowEtagHeaderFilter.disableContentCaching(request.getRequest());
+
+        String ETag = getETag();
+
+        if(request.checkNotModified(ETag)){
+            return null;
+        }
+
         Page<Product> products = productService.getAllProductsPaged(pageable);
 
         return ResponseEntity
                 .ok()
-                .cacheControl(CacheControl.maxAge(1, TimeUnit.MINUTES))
+                .cacheControl(CacheControl
+                        .maxAge(1, TimeUnit.MINUTES)
+                        .cachePublic())
+                .eTag(ETag)
                 .body(products);
 
     }
+
 
     @PostMapping
     public ResponseEntity<Product> saveProduct(@RequestBody ProductInput productInput){
@@ -38,6 +59,22 @@ public class ProductController {
         return ResponseEntity
                 .created(uri)
                 .body(product);
+    }
+
+    private String getETag() {
+        String ETag = "0";
+
+        ZoneId zoneId = ZoneId.of("America/Sao_Paulo");
+        ZonedDateTime zdt = ZonedDateTime.now(zoneId);
+        ZoneOffset zoneOffset = zdt.getOffset();
+
+        LocalDateTime lasCreatedProductDate = productService.getLastCreatedProduct();
+
+        if(lasCreatedProductDate != null){
+            ETag = String.valueOf(
+                    lasCreatedProductDate.toEpochSecond(zoneOffset));
+        }
+        return ETag;
     }
 
 }
